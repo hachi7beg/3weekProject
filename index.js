2ca948b763b35539f047c912da7b97c2e31550de
YukiIWAMA <yuki.iwama@woven.toyota>
2023-08-24 23:00:05 +0900

    feat: Added the feature to generate a random matrix on loading

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ class Timer {
 	constructor() {
 		this.active = false;
 		this.intervalId;
-		this.remberNum = 0;
+		this.prevIdx = 0;
 	}
 	start = (num) => {
 		if (!this.active) {
-			this.intervalId = setInterval(this.randomCellNum, 100, num);
+			this.intervalId = setInterval(this.changeCellColor, 100, num);
 			this.active = true;
-			console.log(this.intervalId);
 		}
 	};
-	randomCellNum = (cellnum) => {
-		const randomNum = Math.floor(Math.random() * cellnum);
-		if (this.remberNum == randomNum) {
-			this.randomCellNum(cellnum);
+	changeCellColor = (numOfCell) => {
+		const idx = Math.floor(Math.random() * numOfCell);
+		if (this.prevIdx == idx) {
+			this.changeCellColor(numOfCell);
 		} else {
-			document.getElementById("cell-" + randomNum).style.backgroundColor =
-				"#FFA500";
-			document.getElementById("cell-" + this.remberNum).style.backgroundColor =
+			document.getElementById("cell-" + idx).style.backgroundColor = "#FFA500";
+			document.getElementById("cell-" + this.prevIdx).style.backgroundColor =
 				"#FFF";
-			this.remberNum = randomNum;
+			this.prevIdx = idx;
 		}
 	};
 	stop = () => {
@@ -31,7 +29,7 @@ class Timer {
 	};
 	reset = () => {
 		if (this.intervalId == 0 && this.active) {
-			document.getElementById("cell-" + this.remberNum).style.backgroundColor =
+			document.getElementById("cell-" + this.prevIdx).style.backgroundColor =
 				"#FFF";
 			this.active = false;
 		}
@@ -39,19 +37,35 @@ class Timer {
 }
 
 class Square {
-	constructor() {
-		this.values = [
-			[1, 2, 3],
-			[4, 5, 6],
-			[7, 8, 9],
-		];
+	#maxNumberOfCell = 36;
+	values = [];
+	numOfCell = 0;
+	constructor(row, col) {
+		if (row * col > this.#maxNumberOfCell) {
+			console.error(
+				`Matrix size (row * col) must be smaller than or equal ${
+					this.#maxNumberOfCell
+				}!`
+			);
+		} else {
+			this.numOfCell = row * col;
+			const cellValueList = [...Array(this.numOfCell)].map((_, i) => i + 1);
+			for (let i = 0; i < row; i++) {
+				const list = [];
+				for (let j = 0; j < col; j++) {
+					const idx = Math.floor(Math.random() * cellValueList.length);
+					list.push(cellValueList.splice(idx, 1)[0]);
+				}
+				this.values.push(list);
+			}
+		}
 		this.timer = new Timer();
 	}
 	getValues = () => {
 		return this.values;
 	};
 	startTimer = () => {
-		this.timer.start(9);
+		this.timer.start(this.numOfCell);
 	};
 	stopTimer = () => {
 		this.timer.stop();
@@ -62,24 +76,22 @@ class Square {
 }
 
 window.onload = () => {
-	const startButton = document.getElementById("start");
 	const table = document.getElementById("table");
-	console.log(table); // ??
-	const square = new Square();
-	const getValue = square.getValues();
-	console.log(getValue);
-	createTableElement(getValue, table);
-	startButton.addEventListener("click", square.startTimer);
+	const startButton = document.getElementById("start");
 	const stopButton = document.getElementById("stop");
-	stopButton.addEventListener("click", square.stopTimer);
 	const resetButton = document.getElementById("reset");
+	const square = new Square(6, 5);
+	const matrix = square.getValues();
+	createTableElement(matrix, table);
+	startButton.addEventListener("click", square.startTimer);
+	stopButton.addEventListener("click", square.stopTimer);
 	resetButton.addEventListener("click", square.resetTimer);
 };
 
-function createTableElement(getValue, table) {
-	for (value of getValue) {
+function createTableElement(matrix, table) {
+	for (row of matrix) {
 		const tr = document.createElement("tr");
-		for (num of value) {
+		for (num of row) {
 			const td = document.createElement("td");
 			td.innerText = num;
 			td.id = "cell-" + String(num - 1);
